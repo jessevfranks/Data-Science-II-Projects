5163f2c6a451719f5c90413fd659ccf235f1fd95
Jesse Franks <jessevfranks@gmail.com>
2026-03-26 16:50:41 -0400

    Added hyper param tuning

--- src/main/scala/project2/Project2Utils.scala
+++ src/main/scala/project2/Project2Utils.scala
@@ -1,12 +1,16 @@
 package project2
 
 import scalation.mathstat.*
+import scalation.modeling.ActivationFun.{f_id, f_sigmoid}
 import scalation.modeling.neuralnet.{NeuralNet_2L, NeuralNet_3L}
-import scalation.modeling.Regression
+import scalation.modeling.{AFF, Regression, qk}
 import scalation.modeling.neuralnet.Optimizer
-import scalation.modeling.qk
 
 class Project2Utils(x: MatrixD, y: VectorD, fname: Array[String]) {
+
+  // Hyper-parameter tuning helpers
+  def setEta(eta: Double): Unit = Optimizer.hp("eta") = eta
+  def setBatchSize(bSize: Int): Unit = Optimizer.hp("bSize") = bSize.toDouble
 
   // Convert VectorD -> MatrixD for NeuralNet_3L 
   private val yMat: MatrixD = MatrixD.fromVector(y) // y requires matrixD here, and MatrixD.fromVector(y) is the correct conversion method.
@@ -17,8 +21,10 @@ class Project2Utils(x: MatrixD, y: VectorD, fname: Array[String]) {
   //    ScalaTion provides .perceptron specifically to accept a VectorD and handle the conversion to MatrixD internally.
 
   // 1. 2L Neural Network must use .perceptron factory since y is a single VectorD
-  def runNeuralNet2L(): Unit = {
-    val mod = NeuralNet_2L.perceptron(x, y, fname)
+  def runNeuralNet2L(eta: Double = 0.1, bSize: Int = 20, f: AFF = f_sigmoid): Unit = {
+    setEta(eta)
+    setBatchSize(bSize)
+    val mod = NeuralNet_2L.perceptron(x, y, fname, f = f)
 
     println("NeuralNet_2L: In-Sample")
     mod.trainNtest()()
@@ -29,8 +35,11 @@ class Project2Utils(x: MatrixD, y: VectorD, fname: Array[String]) {
   }
 
   // 2. 3L Neural Network must use .rescale factory with MatrixD response
-  def runNeuralNet3L(): Unit = {
-    val mod = NeuralNet_3L.rescale(x, yMat, fname)
+  def runNeuralNet3L(eta: Double = 0.1, bSize: Int = 20, f: AFF = f_sigmoid, f1: AFF = f_id, nz: Int = -1): Unit = {
+    setEta(eta)
+    setBatchSize(bSize)
+    
+    val mod = NeuralNet_3L.rescale(x, yMat, fname, nz, f = f, f1 = f1)
 
     println("NeuralNet_3L: In-Sample")
     mod.trainNtest()()
@@ -39,10 +48,6 @@ class Project2Utils(x: MatrixD, y: VectorD, fname: Array[String]) {
     println("NeuralNet_3L: TT Split 80/20")
     mod.validate()()
   }
-
-  // Hyper-parameter tuning helpers
-  def setEta(eta: Double): Unit = Optimizer.hp("eta") = eta
-  def setBatchSize(bSize: Int): Unit = Optimizer.hp("bSize") = bSize.toDouble
 
   // (extra not sure if we need)Feature Selection (uses Regression since NN forwardSelAll needs MatrixD y)
   def runForwardSelect(): Unit = {
